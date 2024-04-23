df84dd58c80d27ceff87ea1443a722d242d1a0d2
eyongfm <fmeyong01@yahoo.fr>
2024-04-23 13:45:51 +0200

    fonction afficher_les_membres est créer

---
+++ allmembers.py
@@ -0,0 +1,10 @@
+#créer un fonction avec mon prénom et le course
+print('-----------------------------------')
+print('  Data Engoineers DevOps practice  ')
+print('-----------------------------------')
+list_of_members = ['Moses Eyong']
+def afficher_les_membres():
+    for lis in list_of_members:
+        print(lis)
+
+afficher_les_membres()
